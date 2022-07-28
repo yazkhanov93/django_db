516085fb83e7b7c7de04f29b51141f2702c784e8
yazkhanov93 <allaberdi16yazhanow@gmail.com>
2022-07-28 12:03:18 +0500

    account app created

---
+++ account/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+
+class CustomUserManager(UserManager):
+
+    def _create_user(sefl, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Email required")
+        email = self.normolize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email,password, **extra_fields)
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = ''
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        return self.name
